a517d72fb1e13489f28e301b5688067b1f175a13
Erik Kuipers <ekuipers1@gmail.com>
2020-10-12 10:05:42 +0200

    Initial Commit

---
+++ BPDX/BPDXApp.swift
@@ -0,0 +1,17 @@
+//
+//  BPDXApp.swift
+//  BPDX
+//
+//  Created by Erik Kuipers on 12.10.20.
+//
+
+import SwiftUI
+
+@main
+struct BPDXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
